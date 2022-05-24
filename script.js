2dfb755152ac8b2b2a669dc24ffff3cf5a1ae15f
supern3on <106067391+supern3on@users.noreply.github.com>
2022-05-24 15:30:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-var happiness = 100;
-var hunger = 100;
+var happiness = 5;
+var hunger = 5;
 var money = 0;
 var moneyclick = 1;
+var alive = true;
 
 setInterval(function() {
-    happiness = --happiness;
-    document.getElementById("happiness").innerHTML = happiness;
+    if (happiness > 0 && alive == true) {
+        happiness = --happiness;
+        document.getElementById("happiness").innerHTML = happiness;
+    } else {
+        console.log('no')
+    }
 }, 5000)
 setInterval(function() {
-    hunger = --hunger;
-    document.getElementById("hunger").innerHTML = hunger;
+    if (hunger > 0 && alive == true) {
+        hunger = --hunger;
+        document.getElementById("hunger").innerHTML = hunger;
+    } else if (hunger == 0) {
+        alive = false;
+        happiness = 0
+        document.getElementById("happiness").innerHTML = happiness;
+    }
 }, 3000)
 
 function petfloppa() {
-    if (happiness + 5 <= 100) {
+    if (happiness + 5 <= 100 && alive == true) {
         happiness = happiness + 5;
         document.getElementById("happiness").innerHTML = happiness;
     } else {
@@ -21,6 +32,8 @@ function petfloppa() {
     }
 }
 function moneyclik() {
-    money = money + moneyclick;
-    document.getElementById("money").innerHTML = money
+    if (alive == true) {
+        money = money + moneyclick;
+        document.getElementById("money").innerHTML = money
+    }
 }
