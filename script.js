4b8d97cf14d83382de5820772cf1f5c185826130
supern3on <106067391+supern3on@users.noreply.github.com>
2022-05-24 13:09:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+var happiness = 100;
+var hunger = 100;
+var money = 0;
+var moneyclick = 1;
 
+setInterval(function() {
+    happiness = --happiness;
+    document.getElementById("happiness").innerHTML = happiness;
+}, 5000)
+setInterval(function() {
+    hunger = --hunger;
+    document.getElementById("hunger").innerHTML = hunger;
+}, 3000)
+
+function petfloppa() {
+    if (happiness + 5 <= 100) {
+        happiness = happiness + 5;
+        document.getElementById("happiness").innerHTML = happiness;
+    } else {
+        console.log("no")
+    }
+}
+function moneyclik() {
+    money = money + moneyclick;
+    document.getElementById("money").innerHTML = money
+}
